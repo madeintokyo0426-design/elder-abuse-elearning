bbf223def7cd0bd1edc4562933d7ca3cd09c8327
madeintokyo0426-design <made.in.tokyo0426@gmail.com>
2025-10-02 21:30:50 +0900

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "鬮倬ｽ｢閠・剞蠕・亟豁｢繝ｻ讓ｩ蛻ｩ謫∬ｭｷ e-learning",
+  description: "遉ｾ蜀・髄縺粗-learning・亥・20蝠上°繧・5蝠上Λ繝ｳ繝繝蜃ｺ鬘鯉ｼ・,
+};
+
+import "./globals.css";
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
